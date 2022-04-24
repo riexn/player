7f911fe7ffc8d260a48d9fd9ba338d00493bd4a8
unknown <riexn>
2022-04-24 13:00:11 -0700

    changes news path to local

--- news.py
+++ news.py
@@ -1,4 +1,3 @@
 from startPlaylist import startPlaylist
-# anything starting with \N must be escaped like this:
-# \\N
-startPlaylist("Z:\\News")
+# any \ must be escaped with another \ like this -> \\
+startPlaylist("C:\\videos\\News")
